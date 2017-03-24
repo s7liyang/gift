ce97313ef4de5d5c6ec439ba9ef16ae9805d1b6e
peanut-PC\peanut <767478876@qq.com>
2017-03-24 18:10:28 +0800

    新增发现、登录、注册页面

--- src/entry/index.js
+++ src/entry/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const enterJs = {
   old: path.resolve(__dirname, '../page/main/js/old.js'),
   index: path.resolve(__dirname, '../page/main/js/index.js'),
+  game: path.resolve(__dirname, '../page/main/js/game.js'),
   me: path.resolve(__dirname, '../page/main/js/me.js'),
 };
 
