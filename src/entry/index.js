1e1fa4df1f68bb6c4af96509aa92168ee3dbdc26
peanut-PC\peanut <767478876@qq.com>
2017-03-28 18:15:22 +0800

    3.28code

--- src/entry/index.js
+++ src/entry/index.js
@@ -6,6 +6,9 @@ const enterJs = {
   index: path.resolve(__dirname, '../page/main/js/index.js'),
   game: path.resolve(__dirname, '../page/main/js/game.js'),
   me: path.resolve(__dirname, '../page/main/js/me.js'),
+  cart: path.resolve(__dirname, '../page/main/js/cart.js'),
+  details: path.resolve(__dirname, '../page/main/js/details.js'),
+  list: path.resolve(__dirname, '../page/main/js/list.js'),
 };
 
 module.exports = enterJs;
